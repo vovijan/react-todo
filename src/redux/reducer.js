e4135c844afce670b7d7b26ed21b57a9c6063ca7
Ivan Gubanov <morelessequal@gmail.com>
2019-04-16 11:45:32 +0300

    add redux reducer for add group and edit group

---
+++ src/redux/reducer.js
@@ -0,0 +1,58 @@
+import { ADD_GROUP, EDIT_GROUP } from './constants';
+import { stat } from 'fs';
+
+const initialState = {
+  data: [
+    {
+      id: 1,
+      name: 'Work',
+      tasks: [
+        {
+          id: 2,
+          name: 'Finish project',
+          completed: false
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Project',
+      tasks: [
+        {
+          id: 3,
+          name: 'Finish project',
+          completed: false
+        }
+      ]
+    }
+  ]
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_GROUP:
+      return {
+        ...state,
+        data: [
+          ...state.data,
+          {
+            id: Date.now(),
+            name: action.payload,
+            tasks: []
+          }
+        ]
+      };
+    case EDIT_GROUP:
+      return {
+        ...state,
+        data: state.data.map(grroup => {
+          if (grroup.id === action.payload.id) {
+            grroup.name = action.payload.name;
+          }
+          return grroup;
+        })
+      };
+    default:
+      return state;
+  }
+};
